5d8b3e623bf9fe491a538fc7e1660c167c2ce92b
Tiran Baghdasaryan <tirbaghdasaryan0808@gmail.com>
2022-07-31 18:31:03 +0400

    Product Configuration

---
+++ Context/Configurations/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Work_with_orders.Entities;
+
+namespace Work_with_orders.Context.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.Property(p => p.Id).UseIdentityAlwaysColumn();
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.Price).IsRequired().HasDefaultValue(0);
+        builder.Property(p => p.Description).IsRequired().HasDefaultValue("None Description");
+
+
+        builder.HasCheckConstraint("CK_Price", "\"Price\" >= 0");
+        builder.HasKey(u => u.Id);
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Work_with_orders.Entities;
+
+public class Product : EntityBase<long>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    
+}
