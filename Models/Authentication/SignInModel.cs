98e03027b7acf982247a6999893a727179dcef9f
Tiran Baghdasaryan <tirbaghdasaryan0808@gmail.com>
2022-07-31 21:10:45 +0400

    Authetication Models

---
+++ Models/Authentication/SignInModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Work_with_orders.Models.Authentication;
+
+public class SignInModel
+{
+    [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Must be a valid Email Address")]
+    [Required]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; }
+}
