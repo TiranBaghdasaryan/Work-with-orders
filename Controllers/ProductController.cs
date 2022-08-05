fe9edcdb480c301197b8d6bb333f03d89f715c73
Tiran Baghdasaryan <tirbaghdasaryan0808@gmail.com>
2022-08-05 14:18:38 +0400

    bugfixed concurrent requests

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,10 +1,14 @@
-﻿using AutoMapper;
+﻿using System.Runtime.CompilerServices;
+using System.Transactions;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Work_with_orders.Context;
 using Work_with_orders.Entities;
 using Work_with_orders.Models.Product;
 using Work_with_orders.Repositories;
+using IsolationLevel = System.Data.IsolationLevel;
 
 namespace Work_with_orders.Controllers;
 
@@ -16,11 +20,12 @@ public class ProductController : ControllerBase
     private readonly IMapper _mapper;
     private readonly ApplicationContext _context;
 
-    public ProductController(ProductRepository productRepository, IMapper mapper, ApplicationContext applicationContext)
+    public ProductController(ProductRepository productRepository, IMapper mapper, ApplicationContext applicationContext,
+        ApplicationContext context)
     {
         _productRepository = productRepository;
         _mapper = mapper;
-        _context = applicationContext;
+        _context = context;
     }
 
     [HttpGet]
@@ -82,5 +87,71 @@ public class ProductController : ControllerBase
         _productRepository.Delete(product);
         await _productRepository.Save();
         return Ok("The product was successfully deleted.");
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost("{id}/quantity")]
+    public async Task<IActionResult> AddProductQuantity(long id, [FromBody] int quantity)
+    {
+        var product = await _productRepository.GetById(id);
+        if (Equals(product, null))
+        {
+            return BadRequest("The product does not exist.");
+        }
+
+        product.Quantity += quantity;
+        await _productRepository.Save();
+        return Ok("The product was added successfully.");
+    }
+
+    private static object _lock = new object();
+
+    //[Authorize(Roles = "Admin")]
+    [HttpDelete("{id}/quantity")]
+    public async Task<IActionResult> RemoveProductQuantity(long id, [FromBody] int quantity)
+    {
+        await using (var transaction = _context.Database.BeginTransaction(IsolationLevel.ReadCommitted))
+        {
+            try
+            {
+                // var product = await _productRepository.GetById(id);
+                // if (Equals(product, null))
+                // {
+                //     return BadRequest("The product does not exist.");
+                // }
+                // product.Quantity -= quantity;
+                // await _productRepository.Save();
+                // await transaction.CommitAsync();
+
+                Product product;
+                lock (_lock)
+                {
+                    product = (_productRepository.GetById(id).Result)!;
+
+                    if (Equals(product, null))
+                    {
+                        return BadRequest("The product does not exist.");
+                    }
+
+                    product.Quantity -= quantity;
+                    _productRepository.SaveSync();
+                    transaction.Commit();
+                }
+
+                // return Ok(product.Quantity);
+
+                return Ok("The product was successfully deleted.");
+            }
+            catch (Exception exception)
+            {
+                await transaction.RollbackAsync();
+                if (exception.InnerException != null && exception.InnerException.Message.Contains("CK_Quantity"))
+                {
+                    return BadRequest("So many products are not in warehouse.");
+                }
+
+                throw;
+            }
+        }
     }
 }
